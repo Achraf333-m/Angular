8dd6277c86897ab9f9699ecfa74e286d069d1e16
Achraf333-m <achrafdaimallah2003@gmail.com>
2023-06-28 01:28:34 -0400

    small angular project

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { UiService } from 'src/app/services/ui.service';
+import {Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css'],
+})
+export class AddTaskComponent {
+  @Output() onAddTask:EventEmitter<Task> = new EventEmitter()
+  
+  text: string | any;
+  day: string | any;
+  reminder: boolean = false;
+  showAddTask: boolean | any;
+  subscription: Subscription | any;
+  
+  constructor(private uiService:UiService) {
+    this.subscription = this.uiService.onToggle().subscribe((value) => this.showAddTask = value)
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    if (!this.text || !this.day) {
+      alert('Please add all the needed informations');
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+    };
+
+    this.onAddTask.emit(newTask)
+
+    this.text = '',
+    this.day = '',
+    this.reminder = false
+
+
+  }
+
+}
---
+++ src/app/mock-task.ts
@@ -0,0 +1,33 @@
+import {Task} from "./Task"
+
+
+
+export const TASKS:Task[] = [
+  {
+    id: 1,
+    text: "Doc's appointment",
+    day: 'June 27th at 10:00am',
+    reminder: true,
+  },
+
+  {
+    id: 2,
+    text: "grandma's visit",
+    day: 'June 28th  at 11:00am',
+    reminder: true,
+  },
+
+  {
+    id: 3,
+    text: 'Meeting at work',
+    day: 'June 30th at 9:00am',
+    reminder: false,
+  },
+
+  {
+    id: 4,
+    text: 'Go to gym',
+    day: 'July 1st at 6:00pm',
+    reminder: true,
+  },
+];
